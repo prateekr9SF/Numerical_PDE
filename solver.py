b0f289bc14a2ce6f29c95c438206870a6c158266
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2023-11-02 20:23:22 -0400

    add place-holder for cg

--- solver.py
+++ solver.py
@@ -36,3 +36,5 @@ def solveFD2_Poissons(nx, ny, dx, dy, f, size, u):
                 u[i, j] = (dx**2 * dy**2 * f[i, j] + dy**2 * (un[i - 1, j] + un[i + 1, j]) + dx**2 * (un[i, j - 1] + un[i, j + 1])) / (2 * (dx**2 + dy**2))
     return u
 
+
+    # Define place holder for conjugate gradient solvers
